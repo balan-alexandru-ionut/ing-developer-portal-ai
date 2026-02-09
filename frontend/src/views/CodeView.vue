29718d7f233b361c9a4ae7f43bd58a04bbce0d1b
Alexandru Ionut Balan <alexandru-ionut.balan@ing.com>
2026-02-09 13:14:46 +0200

    feat(codeview): add download archive functionality and update dependencies
    Signed-off-by: Alexandru Ionut Balan <alexandru-ionut.balan@ing.com>

--- frontend/src/views/CodeView.vue
+++ frontend/src/views/CodeView.vue
@@ -5,6 +5,7 @@ import {codeToHtml} from "shiki";
 import {ref, watchEffect} from "vue";
 import type {File} from "@/types/files.ts";
 import FileTree from "@/components/FileTree.vue";
+import {Download, Play} from "lucide-vue-next";
 
 const generationStore = useGenerationStore()
 const renderedFiles = ref<Map<string, string>>(new Map())
@@ -15,6 +16,21 @@ async function renderFile(file: File): Promise<string> {
     lang: generationStore.language.toLowerCase(),
     theme: 'catppuccin-mocha',
   })
+}
+
+function downloadArchive() {
+  fetch('/api/generate/archive', {method: 'GET', headers: {'Accept': 'application/zip'}})
+    .then(response => response.blob())
+    .then(blob => {
+      const url = globalThis.URL.createObjectURL(blob)
+      const a = document.createElement('a')
+      a.href = url
+      a.download = 'archive.zip'
+      document.body.appendChild(a)
+      a.click()
+      a.remove()
+      globalThis.URL.revokeObjectURL(url)
+    })
 }
 
 watchEffect(async () => {
@@ -38,6 +54,16 @@ function fileOpen(filePath: string) {
   <div class="flex gap-8 p-12 bg-base-300 h-screen w-full">
     <FileTree @fileOpen="path => fileOpen(path)" :filePaths="generationStore.files.map(f => f.filePath)" />
     <div class="mockup-window bg-base-100 border border-base-300 size-full overflow-scroll">
+      <div class="flex justify-end gap-4 w-full p-4">
+        <button class="btn btn-primary" @click="downloadArchive">
+          <Download />
+          Download
+        </button>
+        <button class="btn btn-disabled">
+          Run
+          <Play />
+        </button>
+      </div>
       <div class="p-4 rounded-lg" v-html="visibleFile"></div>
     </div>
   </div>
